46ea4ff29bb8d01be43c16dfe49c47abbf6e3b50
BhagyaRanaweera <sithubhagya01@gmail.com>
2024-11-07 09:53:06 -0800

    changes update

--- src/main/java/com/example/StudentMangementSystem/StudentMangementSystemApplication.java
+++ src/main/java/com/example/StudentMangementSystem/StudentMangementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentMangementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(S.class, args);
+		SpringApplication.run(StudentMangementSystemApplication.class, args);
 	}
 
 }
